697fb04a9102b979fbe89be4533faa93d4e9ec84
Victoria-20 <victoriaocon111@gmail.com>
2023-01-04 13:24:49 +0300

    Fix issues

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,11 @@
 export const weakMap = new WeakMap();
 let count = 1;
 
-export function queryAPI(obj) {
-  weakMap.set(obj, count);
+export function queryAPI(endPoint) {
+  weakMap.set(endPoint, count);
   count += 1;
-
-  const qcount = weakMap.get(obj);
-
-  if (qcount >= 5) { throw new Error('Endpoint load is high'); }
+  const queryCount = weakMap.get(endPoint);
+  if (queryCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
 }
--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,9 @@
 export default function groceriesList() {
-  const groceries = new Map([
+  return new Map([
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
   ]);
-  return groceries;
 }
