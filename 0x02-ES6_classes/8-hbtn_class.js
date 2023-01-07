64c80a91980e7c3f8c6425f34e5712dde2b390d4
Victoria-20 <victoriaocon111@gmail.com>
2023-01-07 13:27:01 +0300

    8. Primitive - Holberton Class

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,14 +1,19 @@
 export default class HolbertonClass {
   constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('Size must be a number');
+    } else if (typeof location !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+
     this._size = size;
     this._location = location;
   }
 
-  [this.toPrimitive](hint) {
-    if (hint === 'string') {
-      return this._location;
-    } if (hint === 'number') {
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
       return this._size;
     }
+    return this._location;
   }
 }
