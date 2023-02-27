7e895bf5ed507173d3e09d8b28d94f58d97e1489
Victoria-20 <victoriaocon111@gmail.com>
2023-02-27 11:34:22 +0300

    Fix issue

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,10 @@
 const app = require('http');
 
+const hostname = '127.0.0.1';
+
 app.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
-}).listen(1245);
+}).listen(1245, hostname);
 
 module.exports = app;
