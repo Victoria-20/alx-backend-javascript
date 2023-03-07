ae27ceaf89c9c3a8d467a3546f13be7fae46c1ec
Victoria-20 <victoriaocon111@gmail.com>
2023-03-07 11:01:20 +0300

    5. Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  //before
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  //after
+  afterEach(() => {
+    expect(consoleSpy.calledOnce).to.be.true;
+    consoleSpy.restore();
+  });
+
+  it('checks output of sendPaymentRequestToApi with 100 and 20 as args', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it('checks output of sendPaymentRequestToApi with 10 and 10 as args', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+  });
+});
+
