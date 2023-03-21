d6b0a4e8b44db10e81dd8d1263b7661ff8f400f9
Victoria-20 <victoriaocon111@gmail.com>
2023-03-21 09:53:48 +0300

    Fix issue

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,26 +1,35 @@
-const fs = require('fs');
+const fs = require('fs'); // Import the 'fs' module for file system operations
 
-function countStudents(filepath) {
-  let data = [];
-  const fields = {};
+function countStudents(path) { // takes a path of a file
   try {
-    data = fs.readFileSync(filepath).toString().split('\n').slice(1);
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-  console.log(`Number of students: ${data.length}`);
-  data.map((student) => student.split(','))
-    .map((student) => {
-      if (!(student[3] in fields)) {
-        fields[student[3]] = [];
+    const data = fs.readFileSync(path, 'utf8'); // read and store in data
+    // console.log(data);
+    const lines = data.trim().split('\n'); // return a clean array separated by new line
+    // console.log(lines);
+    const fields = {}; // keeps track of number of students in each field
+    for (const line of lines) {
+      const values = line.split(','); // iterate through each line and store the array in values
+      // console.log(values)
+      if (values.length === 4 && values[0] !== 'firstname') {
+        const field = values[3].trim(); // the field section
+        // console.log(field);
+        if (fields[field]) { // if the field exsists
+          fields[field].push(values[0]); // add the student name to the field
+        } else {
+          fields[field] = [values[0]]; // create a new field with a student name
+        }
       }
-      return fields[student[3]].push(student[0]);
-    });
-  for (const key in fields) {
-    if (Object.prototype.hasOwnProperty.call(fields, key)) {
-      console.log((`Number of students in ${key}: ${fields[key].length}.\
-        List: ${fields[key]}`).replace(/,/g, ', '));
     }
+    // console.log(lines.length)
+    console.log(`Number of students: ${lines.length - 1}`);
+    for (const field in fields) { // iterate through each field in fields
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        const list = fields[field]; // get the list of students in the current field
+        console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
   }
 }
 
